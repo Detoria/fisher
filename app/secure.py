0eb8e1a9002a19e2e8418f49da0055aa9e850d4e
detoria <huangzewei_job@163.com>
2018-05-09 00:52:26 +0800

    add secure.py + setting.py

---
+++ app/secure.py
@@ -0,0 +1,6 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:hzw0527@localhost:3306/fisher'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_COMMIT_TEARDOWN = True
+SECRET_KEY = 'uhqwdxxsdfrtghskwhflakskjhrhchfhr'
